e515a10530b886545e0529bb785883cde1c35bea
jdjddjd094-dot <jdjddjd094@gmail.com>
2025-10-21 08:52:30 +0300

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,7 +3,7 @@ class AppConstants {
   static const String tmdbBaseUrl = 'https://api.themoviedb.org/3';
   static const String tmdbImageBaseUrl = 'https://image.tmdb.org/t/p/w500';
   static const String tmdbOriginalImageBaseUrl = 'https://image.tmdb.org/t/p/original';
-  static const String tmdbAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTc2MDk3MTlhNTYxYjM0MWM4MDYyYzMzN2FiZTM5NyIsIm5iZiI6MTc0NDI5MzUwOC4xMDQsInN1YiI6IjY3ZjdjZTg0MzE3NzUyNzZkNmQ5OTM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jB-LdCFKnX7xETXv3UgAHXffgoCOFK9wfyr6Z8y4AzI';
+  static const String tmdbAccessToken = 'YOUR_TMDB_ACCESS_TOKEN_HEAR';
   
   // SuperEmbed
   static const String superEmbedBaseUrl = 'https://multiembed.mov';
